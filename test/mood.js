c5a7dc78340a095ff569f2509767ac8d4067a049
bredele <olivier.wietrich@gmail.com>
2016-11-20 12:24:27 -0700

    started refactoring tests

--- test/mood.js
+++ test/mood.js
@@ -4,3 +4,13 @@
 
 var test = require('tape')
 var mood = require('..')
+
+
+test('should be an emitter', assert => {
+  assert.plan(4)
+  var states = mood()
+  assert.equal(typeof states.on, 'function')
+  assert.equal(typeof states.off, 'function')
+  assert.equal(typeof states.once, 'function')
+  assert.equal(typeof states.emit, 'function')
+})
