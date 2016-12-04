6d14c6e8fd142afef2a879ee4d9dde4df20d4ad8
bredele <olivier.wietrich@gmail.com>
2016-12-03 20:35:36 -0700

    add multiple conditions for a transition

--- index.js
+++ index.js
@@ -8,12 +8,19 @@ var gobs = require('gobs')
 module.exports = function(state) {
   var store = {}
   return {
-    add: function(before, topic, transition, after) {
-
+    add: function(before, conditions, transition, after) {
+      var clone = conditions.slice(0)
+      var arr = store[before] = (store[before] || [])
+      arr.push(gobs([].concat(conditions), function() {
+        transition.apply(null, arguments)
+        state = after
+        conditions.concat(clone)
+      }))
     },
 
-    emit: function(topic) {
-
+    emit: function() {
+      var transition = store[state][0]
+      transition.apply(null, arguments)
     }
   }
 }
--- test/mood.js
+++ test/mood.js
@@ -6,9 +6,18 @@ var test = require('tape')
 var mood = require('..')
 
 
-test('it should add a state', assert => {
+test('it should trigger a state transition with a single condition', assert => {
   assert.plan(1)
   var states = mood('init')
-  states.add('init', 'hello', () => assert.pass())
+  states.add('init', 'hello', () => assert.pass('simple condition'))
   states.emit('hello')
+})
+
+test('it should trigger a state transition with a multiple conditions', assert => {
+  assert.plan(1)
+  var states = mood('init')
+  states.add('init', ['hello', 'world'], () => assert.pass('multiple conditions'))
+  states.emit('hello')
+  states.emit('hello again')
+  states.emit('world')
 })
