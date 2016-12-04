b76a98b6ba15919bc7e7f5bbde121696af7914d7
bredele <olivier.wietrich@gmail.com>
2016-12-03 20:47:54 -0700

    extract add method for future polymorphism

--- index.js
+++ index.js
@@ -6,27 +6,30 @@ var gobs = require('gobs')
 
 
 module.exports = function(state) {
-  var store = {}
   var that = {
-    current: state,
-    add: function(before, conditions, transition, after) {
-      if(typeof transition == 'string') {
-        after = transition
-        transition = null
-      }
-      var clone = conditions.slice(0)
-      var arr = store[before] = (store[before] || [])
-      arr.push(gobs([].concat(conditions), function() {
-        transition && transition.apply(null, arguments)
-        that.current = after
-        conditions.concat(clone)
-      }))
-    },
+    current: state
+  }
 
-    emit: function() {
-      var transition = store[that.current][0]
-      transition.apply(null, arguments)
+  var store = {}
+
+  var add = function(before, conditions, transition, after) {
+    if(typeof transition == 'string') {
+      after = transition
+      transition = null
     }
+    var clone = conditions.slice(0)
+    var arr = store[before] = (store[before] || [])
+    arr.push(gobs([].concat(conditions), function() {
+      transition && transition.apply(null, arguments)
+      that.current = after
+      conditions.concat(clone)
+    }))
+  }
+
+  that.add = add
+  that.emit = function() {
+    var transition = store[that.current][0]
+    transition.apply(null, arguments)
   }
   return that
 }
