a41b896fa235cf6f9c6da4fcc5c58236af2df6ef
bredele <olivier.wietrich@gmail.com>
2020-03-10 10:51:08 -0600

    Initialize state machine with state map

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 
 
-module.exports = (init) => {
-  let state = init
+module.exports = (init, map) => {
+  let state = current(map || init)
   return {
     state() {
       return state
     }
+  }
+}
+
+/**
+ * Get current state from
+ *
+ * @param {String|Function|Object} state
+ * @return {String} (null if not passed)
+ * @private
+ */
+
+function current (state) {
+  const type = typeof state
+  switch (type) {
+    case 'string':
+      return state
+    case 'object':
+      return Object.keys(state)[0]
   }
 }
