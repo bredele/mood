65080738d05b35ae4275ab83de78b29a6d129113
bredele <olivier.wietrich@gmail.com>
2020-03-10 10:45:16 -0600

    Should initialize state machine with state map

--- test/mood.js
+++ test/mood.js
@@ -10,3 +10,12 @@ test('shoud initialize state machine with state', assert => {
   const machine = mood('init')
   assert.equal(machine.state(), 'init')
 })
+
+test('should initialize state machine with state map', assert => {
+  assert.plan(1)
+  const machine = mood({
+    'init': [],
+    'something': []
+  })
+  assert.equal(machine.state(), 'init')
+})
